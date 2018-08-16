009da58e2d70c9ca3a34f995184b897c2c0be2e4
biaedwards <bibby.georgieva@gmail.com>
2018-08-16 12:25:48 +0300

    Created database schema.

---
+++ extensionRepositoryDB.sql
@@ -0,0 +1,92 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.3.8-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for extension_repository
+CREATE DATABASE IF NOT EXISTS `extension_repository` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `extension_repository`;
+
+-- Dumping structure for table extension_repository.authorities
+CREATE TABLE IF NOT EXISTS `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `username_authority` (`username`,`authority`),
+  CONSTRAINT `FK__users` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table extension_repository.authorities: ~0 rows (approximately)
+/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
+
+-- Dumping structure for table extension_repository.extensions
+CREATE TABLE IF NOT EXISTS `extensions` (
+  `ExtensionID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Owner` varchar(50) NOT NULL,
+  `Downloads` int(11) NOT NULL DEFAULT 0,
+  `File` blob NOT NULL,
+  `Link` varchar(200) NOT NULL,
+  PRIMARY KEY (`ExtensionID`),
+  KEY `FK_extensions_users` (`Owner`),
+  CONSTRAINT `FK_extensions_users` FOREIGN KEY (`Owner`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table extension_repository.extensions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `extensions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `extensions` ENABLE KEYS */;
+
+-- Dumping structure for table extension_repository.extension_tag
+CREATE TABLE IF NOT EXISTS `extension_tag` (
+  `ExtensionID` int(11) DEFAULT NULL,
+  `TagID` int(11) DEFAULT NULL,
+  KEY `FK__extensions` (`ExtensionID`),
+  KEY `FK__tags` (`TagID`),
+  CONSTRAINT `FK__extensions` FOREIGN KEY (`ExtensionID`) REFERENCES `extensions` (`ExtensionID`),
+  CONSTRAINT `FK__tags` FOREIGN KEY (`TagID`) REFERENCES `tags` (`TagID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table extension_repository.extension_tag: ~0 rows (approximately)
+/*!40000 ALTER TABLE `extension_tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `extension_tag` ENABLE KEYS */;
+
+-- Dumping structure for table extension_repository.tags
+CREATE TABLE IF NOT EXISTS `tags` (
+  `TagID` int(11) NOT NULL,
+  `TagName` varchar(50) NOT NULL,
+  PRIMARY KEY (`TagID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table extension_repository.tags: ~0 rows (approximately)
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+
+-- Dumping structure for table extension_repository.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(68) NOT NULL,
+  `enabled` tinyint(4) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table extension_repository.users: ~3 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
+	('gosho', '{noop}pass2', 1),
+	('misho', '{noop}pass3', 1),
+	('pesho', '{noop}pass1', 1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
