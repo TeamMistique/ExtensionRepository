b72fee9267fdf7602dd0e9bd2f72029200ed2658
RadikMih <radik.mihnev@gmail.com>
2018-08-19 17:09:49 +0300

    updating database -removing status table and adding featured field in the extension table

--- extensionRepositoryDB.sql
+++ extensionRepositoryDB.sql
@@ -11,10 +11,73 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Dumping database structure for extension_repository
+DROP DATABASE IF EXISTS `extension_repository`;
+CREATE DATABASE IF NOT EXISTS `extension_repository` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `extension_repository`;
+
+-- Dumping structure for table extension_repository.authorities
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE IF NOT EXISTS `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `username_authority` (`username`,`authority`),
+  CONSTRAINT `FK__users` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+-- Dumping structure for table extension_repository.extensions
+DROP TABLE IF EXISTS `extensions`;
+CREATE TABLE IF NOT EXISTS `extensions` (
+  `ExtensionID` int(11) NOT NULL AUTO_INCREMENT,
+  `Published` tinyint(1) NOT NULL DEFAULT 0,
+  `Name` varchar(50) NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Owner` varchar(50) NOT NULL,
+  `Downloads` int(11) NOT NULL DEFAULT 0,
+  `File` blob NOT NULL,
+  `Link` varchar(200) NOT NULL,
+  `Issues` int(11) NOT NULL DEFAULT 0,
+  `PullRequests` int(11) NOT NULL DEFAULT 0,
+  `LastCommit` date NOT NULL,
+  `Featured` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`ExtensionID`),
+  KEY `FK_extensions_users` (`Owner`),
+  CONSTRAINT `FK_extensions_users` FOREIGN KEY (`Owner`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+-- Dumping structure for table extension_repository.extension_tag
+DROP TABLE IF EXISTS `extension_tag`;
+CREATE TABLE IF NOT EXISTS `extension_tag` (
+  `ExtensionID` int(11) DEFAULT NULL,
+  `TagID` int(11) DEFAULT NULL,
+  KEY `FK_extension_tag_extensions` (`ExtensionID`),
+  KEY `FK_extension_tag_tags` (`TagID`),
+  CONSTRAINT `FK_extension_tag_extensions` FOREIGN KEY (`ExtensionID`) REFERENCES `extensions` (`ExtensionID`),
+  CONSTRAINT `FK_extension_tag_tags` FOREIGN KEY (`TagID`) REFERENCES `tags` (`TagID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+-- Dumping structure for table extension_repository.tags
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `TagID` int(11) NOT NULL AUTO_INCREMENT,
+  `TagName` varchar(50) NOT NULL,
+  PRIMARY KEY (`TagID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+-- Dumping structure for table extension_repository.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(68) NOT NULL,
+  `enabled` tinyint(4) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
