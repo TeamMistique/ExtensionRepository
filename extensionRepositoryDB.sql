d5dc4fbb72f99cc4f9817e440e4519a903a18ca7
biaedwards <bibby.georgieva@gmail.com>
2018-08-24 17:23:57 +0300

    Changed database

--- extensionRepositoryDB.sql
+++ extensionRepositoryDB.sql
@@ -45,23 +45,19 @@ CREATE TABLE IF NOT EXISTS `extensions` (
   `FeaturedDate` date DEFAULT NULL,
   `PublishedDate` date DEFAULT NULL,
   `CreatedDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `ImageID` int(11) NOT NULL,
-  `FileID` int(11) NOT NULL,
+  `Image` varchar(500) NOT NULL,
+  `File` varchar(500) NOT NULL,
   PRIMARY KEY (`ExtensionID`),
   KEY `FK_extensions_users` (`Owner`),
-  KEY `FK_extensions_images` (`ImageID`),
-  KEY `FK_extensions_files` (`FileID`),
-  CONSTRAINT `FK_extensions_files` FOREIGN KEY (`FileID`) REFERENCES `files` (`FileID`),
-  CONSTRAINT `FK_extensions_images` FOREIGN KEY (`ImageID`) REFERENCES `images` (`ImageID`),
   CONSTRAINT `FK_extensions_users` FOREIGN KEY (`Owner`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table extension_repository.extensions: ~3 rows (approximately)
 /*!40000 ALTER TABLE `extensions` DISABLE KEYS */;
-REPLACE INTO `extensions` (`ExtensionID`, `Name`, `Description`, `Owner`, `Downloads`, `Link`, `Issues`, `PullRequests`, `LastCommit`, `FeaturedDate`, `PublishedDate`, `CreatedDate`, `ImageID`, `FileID`) VALUES
-	(1, 'Python', 'Python', 'user', 1000, 'link', 1, 1, '2018-08-22', '2017-08-22', '2018-08-22', '2016-08-22 15:22:43', 1, 1),
-	(2, 'Extension1', 'new one', 'Bibby', 3, 'link1', 1, 0, '2018-08-22', '2018-08-22', '2018-08-24', '2018-08-22 15:26:27', 2, 2),
-	(3, 'Extension 2', 'Extension2', 'Radik', 10, 'link2', 0, 0, '2018-08-22', '2018-08-22', '2018-08-21', '2018-08-22 15:28:14', 3, 3);
+INSERT INTO `extensions` (`ExtensionID`, `Name`, `Description`, `Owner`, `Downloads`, `Link`, `Issues`, `PullRequests`, `LastCommit`, `FeaturedDate`, `PublishedDate`, `CreatedDate`, `Image`, `File`) VALUES
+	(1, 'Python', 'Python', 'user', 0, 'link', 1, 1, '2018-08-22', '2019-08-22', '2018-08-22', '2018-08-22 15:22:43', 'Image1', 'File1'),
+	(2, 'Extension1', 'new one', 'Bibby', 0, 'link1', 1, 0, '2018-08-22', '2018-08-22', NULL, '2018-08-22 15:26:27', 'Image2', 'File2'),
+	(3, 'Extension 2', 'Extension2', 'Radik', 0, 'link2', 0, 0, '2018-08-22', '2018-08-22', '2018-08-22', '2018-08-22 15:28:14', 'Image3', 'File3');
 /*!40000 ALTER TABLE `extensions` ENABLE KEYS */;
 
 -- Dumping structure for table extension_repository.extension_tag
@@ -75,46 +71,11 @@ CREATE TABLE IF NOT EXISTS `extension_tag` (
   CONSTRAINT `FK_extension_tag_tags` FOREIGN KEY (`TagID`) REFERENCES `tags` (`TagID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table extension_repository.extension_tag: ~1 rows (approximately)
+-- Dumping data for table extension_repository.extension_tag: ~0 rows (approximately)
 /*!40000 ALTER TABLE `extension_tag` DISABLE KEYS */;
-REPLACE INTO `extension_tag` (`ExtensionID`, `TagID`) VALUES
+INSERT INTO `extension_tag` (`ExtensionID`, `TagID`) VALUES
 	(1, 2);
 /*!40000 ALTER TABLE `extension_tag` ENABLE KEYS */;
-
--- Dumping structure for table extension_repository.files
-DROP TABLE IF EXISTS `files`;
-CREATE TABLE IF NOT EXISTS `files` (
-  `FileID` int(11) NOT NULL AUTO_INCREMENT,
-  `FileName` varchar(50) NOT NULL,
-  `DownloadUri` varchar(50) NOT NULL,
-  `FileType` varchar(50) NOT NULL,
-  `Size` int(11) NOT NULL,
-  PRIMARY KEY (`FileID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
--- Dumping data for table extension_repository.files: ~3 rows (approximately)
-/*!40000 ALTER TABLE `files` DISABLE KEYS */;
-REPLACE INTO `files` (`FileID`, `FileName`, `DownloadUri`, `FileType`, `Size`) VALUES
-	(1, 'file', 'url', 'doc', 12),
-	(2, 'file1', 'url1', 'doc1', 12),
-	(3, 'file2', 'url2', 'doc2', 12);
-/*!40000 ALTER TABLE `files` ENABLE KEYS */;
-
--- Dumping structure for table extension_repository.images
-DROP TABLE IF EXISTS `images`;
-CREATE TABLE IF NOT EXISTS `images` (
-  `ImageID` int(11) NOT NULL AUTO_INCREMENT,
-  `ImagePath` varchar(200) NOT NULL,
-  PRIMARY KEY (`ImageID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
--- Dumping data for table extension_repository.images: ~3 rows (approximately)
-/*!40000 ALTER TABLE `images` DISABLE KEYS */;
-REPLACE INTO `images` (`ImageID`, `ImagePath`) VALUES
-	(1, 'https://s.hswstatic.com/gif/whiskers-sam.jpg'),
-	(2, 'https://i.gyazo.com/d9902d05c4e956a242a0268d9fe499c7.png'),
-	(3, 'https://i.gyazo.com/fb37f080a8871718a4b628c5ae2e1d05.png');
-/*!40000 ALTER TABLE `images` ENABLE KEYS */;
 
 -- Dumping structure for table extension_repository.tags
 DROP TABLE IF EXISTS `tags`;
@@ -126,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `tags` (
 
 -- Dumping data for table extension_repository.tags: ~2 rows (approximately)
 /*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-REPLACE INTO `tags` (`TagID`, `TagName`) VALUES
+INSERT INTO `tags` (`TagID`, `TagName`) VALUES
 	(1, 'python'),
 	(2, 'debugger');
 /*!40000 ALTER TABLE `tags` ENABLE KEYS */;
@@ -142,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- Dumping data for table extension_repository.users: ~3 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-REPLACE INTO `users` (`username`, `password`, `enabled`) VALUES
+INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
 	('Bibby', '123', 1),
 	('Radik', '123', 1),
 	('user', '123', 1);
