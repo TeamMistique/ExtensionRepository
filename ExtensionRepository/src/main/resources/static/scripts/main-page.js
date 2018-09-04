e5d01687b16213c65c9ef64142a4b7be0d62e84e
RadikMih <radik.mihnev@gmail.com>
2018-09-04 08:40:49 +0300

    replacing extension page with modal one

--- ExtensionRepository/src/main/resources/static/scripts/main-page.js
+++ ExtensionRepository/src/main/resources/static/scripts/main-page.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     fillFeaturedList();
     fillNewList();
     fillAllList();
-    ResCarouselOnInit(); 
+    ResCarouselOnInit();
 });
 
 $('#home-button, #home-button2').on('click', function (e) {
@@ -16,7 +16,7 @@ $('#search-button').on('click', function (e) {
     e.preventDefault();
     $('body > div').addClass('hide');
     $('#search-page').removeClass('hide');
-    
+
 });
 
 var fillPopularList = function () {
@@ -50,7 +50,7 @@ var fillNewList = function () {
 };
 
 var fillMainPageList = function (location, data) {
-       
+
     location.html('');
 
     if (data !== '') {
@@ -80,7 +80,7 @@ var fillAllList = function () {
 };
 
 var fillSearchPageList = function (location, data) {
-       
+
     location.html('');
 
     if (data !== '') {
@@ -99,80 +99,80 @@ var fillSearchPageList = function (location, data) {
     }
 };
 
-$('#featured-container, #popular-container, #new-container, #search-container').on('click', '.col-md-2', function () {
-    console.log("Extension has been clicked - 1.")
-    console.log($(this) + '..................................' + $(this).attr('value'));
-    var id = $(this).attr('value');
-    getExtensionData(id);
-    $('body > div').addClass('hide');
-    $('#one-extension-page').removeClass('hide');
-});
+// $('#featured-container, #popular-container, #new-container, #search-container').on('click', '.col-md-2', function () {
+//     console.log("Extension has been clicked - 1.")
+//     console.log($(this) + '..................................' + $(this).attr('value'));
+//     var id = $(this).attr('value');
+//     getExtensionData(id);
+//     $('body > div').addClass('hide');
+//     $('#one-extension-page').removeClass('hide');
+// });
 
 
-var getExtensionData = function (id) {
-    $.ajax({
-        type: "GET",
-        url: "/api/extensions/" + id,
-        success: function (data) {
-            fillExtensionPage($('#one-extension-page'), data)
-        }
-    });
-};
+// var getExtensionData = function (id) {
+//     $.ajax({
+//         type: "GET",
+//         url: "/api/extensions/" + id,
+//         success: function (data) {
+//             fillExtensionPage($('#one-extension-page'), data)
+//         }
+//     });
+// };
 
-var fillExtensionPage = function (location, extension) {
-    location.html('');
+// var fillExtensionPage = function (location, extension) {
+//     location.html('');
 
-    if (extension !== '') {
-        var html = "";
-        html += '<div class="container justify-content-center" value="' + extension.id + '">';
-        html += '<div class="row justify-content-center "><div class="col-md-7"><h1 class="page-header">' + extension.name + '</h1></div></div>';
-        html += '<div class="row"><div class="col-md-3"><img class="img-responsive" src="' + extension.image + '"></div>';
-        html += '<div class="col-md-2 text-left"><h3>Information</h3><div>' + extension.owner + '</div>';
-        html += '<div><i class="fas fa-download"> </i> ' + extension.downloadsCounter + '</div>';
-        html += '<div>Version' + extension.version + '</div>';
-        html += '<div id="download"><a id="download-button" class="row btn btn-success" type="button" href="' + extension.file + '">Download</a></div></div>';
-        html += '<div class="col-md-2 text-left"><h3 id="github"><a href="' + extension.link + '"><i class="fab fa-github-alt">' + '  ' + '</i>GitHub</a></h3>';
-        html += '<div>Open Issues  ' + extension.issuesCounter + '</div>';
-        html += '<div>Pull Requests  ' + extension.pullRequestsCounter + '</div>';
-        html += '<div>Last commit ' + extension.lastCommitDate + '</div></div></div><div style="margin-bottom: 1%;"></div>';
-        html += '<div class="row"><div class="col-md-7"><p>' + extension.description + '</p></div></div>';
+//     if (extension !== '') {
+//         var html = "";
+//         html += '<div class="container justify-content-center" value="' + extension.id + '">';
+//         html += '<div class="row justify-content-center "><div class="col-md-7"><h1 class="page-header">' + extension.name + '</h1></div></div>';
+//         html += '<div class="row"><div class="col-md-3"><img class="img-responsive" src="' + extension.image + '"></div>';
+//         html += '<div class="col-md-2 text-left"><h3>Information</h3><div>' + extension.owner + '</div>';
+//         html += '<div><i class="fas fa-download"> </i> ' + extension.downloadsCounter + '</div>';
+//         html += '<div>Version' + extension.version + '</div>';
+//         html += '<div id="download"><a id="download-button" class="row btn btn-success" type="button" href="' + extension.file + '">Download</a></div></div>';
+//         html += '<div class="col-md-2 text-left"><h3 id="github"><a href="' + extension.link + '"><i class="fab fa-github-alt">' + '  ' + '</i>GitHub</a></h3>';
+//         html += '<div>Open Issues  ' + extension.issuesCounter + '</div>';
+//         html += '<div>Pull Requests  ' + extension.pullRequestsCounter + '</div>';
+//         html += '<div>Last commit ' + extension.lastCommitDate + '</div></div></div><div style="margin-bottom: 1%;"></div>';
+//         html += '<div class="row"><div class="col-md-7"><p>' + extension.description + '</p></div></div>';
 
-        if (extension.tags.length > 0) {
-            html += '<div class="col-md-6"><h3 class="tags">Tags</h3><div id="tag-section" class="tag-container">';
-            $.each(extension.tags, function (k, v) {
-                var tagsHtml = "";
-                tagsHtml += '<div class="tag border">' + v.tagName + '</div>';
-                html += tagsHtml;
-            });
-        } else {
-            console.log('error');
-        }
-        html += '</div></div></div></div></div>';
+//         if (extension.tags.length > 0) {
+//             html += '<div class="col-md-6"><h3 class="tags">Tags</h3><div id="tag-section" class="tag-container">';
+//             $.each(extension.tags, function (k, v) {
+//                 var tagsHtml = "";
+//                 tagsHtml += '<div class="tag border">' + v.tagName + '</div>';
+//                 html += tagsHtml;
+//             });
+//         } else {
+//             console.log('error');
+//         }
+//         html += '</div></div></div></div></div>';
 
-        location.append(html)
+//         location.append(html)
 
-    };
-}
+//     };
+// }
 
 // ------------------  Carousel ---------------------
 
 function ResCarouselOnInit() {
     ResCarouselSize();
-    $(document).on('click', '.leftLst, .rightLst', function() {
+    $(document).on('click', '.leftLst, .rightLst', function () {
         ResCarousel(this);
     });
-    $(document).on("mouseenter", ".ResHover", function() {
+    $(document).on("mouseenter", ".ResHover", function () {
         $(this).addClass("ResHovered");
     });
 
-    $(document).on("mouseleave", ".ResHover", function() {
+    $(document).on("mouseleave", ".ResHover", function () {
         $(this).removeClass("ResHovered");
     });
 }
 
-$(window).resize(function() {
+$(window).resize(function () {
     var r = new Date();
-    setTimeout(function() {
+    setTimeout(function () {
         ResCarouselResize(r);
     }, 200);
 });
@@ -180,15 +180,14 @@ $(window).resize(function() {
 function ResCarouselSlide(e) {
     var thiss = $(e).find(".rightLst");
     var dataInterval = $(e).attr('data-interval');
-    !isNaN(dataInterval) && $(e).addClass("ResHover") && setInterval(function() {
+    !isNaN(dataInterval) && $(e).addClass("ResHover") && setInterval(function () {
         !(thiss.parent().hasClass("ResHovered")) && ResCarousel(thiss);
     }, +(dataInterval));
 }
 
 function ResCarouselResize() {
     function myfunction() {
-        console.log("resize Works");
-        $('.resCarousel').each(function() {
+        $('.resCarousel').each(function () {
             var divValue = $(this).attr('data-value');
             var itemWidth = $(this).find('.item').width();
             $(this).find(".resCarousel-inner").scrollLeft(divValue * itemWidth);
@@ -201,8 +200,7 @@ function ResCarouselResize() {
 function ResCarouselSize() {
     var t0 = performance.now();
 
-    //    styleCollector0 = styleCollector1 = styleCollector2 = styleCollector3 = "";
-    $('.resCarousel').each(function(index) {
+    $('.resCarousel').each(function (index) {
         var itemsSplit = $(this).attr("data-items").split('-');
         $(this).addClass("ResSlid" + index);
 
@@ -219,37 +217,28 @@ function ResCarouselSize() {
         }
 
         $(this).attr("data-value", "0");
-        //var r = $('body').width();
-        //var it = r >= 1200 ? itemsSplit[3] : r >= 992 ? itemsSplit[2] : r >= 768 ? itemsSplit[1] : itemsSplit[0];
-        //$(this).attr("data-itm", it);
 
         var styleCollector = "@media (max-width:767px){" + styleCollector0 + "}" +
             "@media (min-width:768px){" + styleCollector1 + "}" +
             "@media (min-width:992px){" + styleCollector2 + "}" +
             "@media (min-width:1200px){" + styleCollector3 + "}";
-        //$(this).append("<div class=\"ResStyleManager\"></div>")
+
         $(this).find("style").remove();
         $(this).append("<style>" + styleCollector + "</style>");
         ResCarouselSlide(this);
-
     });
-    //console.log(styleCollector);
-    //$("body").append("<div class=\"ResStyleManager\"></div>")
-    //$('.ResStyleManager').html(null).append("<style>" + styleCollector + "</style>");
     var t1 = performance.now();
-    console.log('Took', (t1 - t0).toFixed(4), 'milliseconds to Size');
 }
 
 //this function used to move the items
 function ResCarousel(Btn) {
-    //var t0 = performance.now();
+
     var parent = $(Btn).parent(),
         slide = +parent.attr("data-slide"),
         itemsDiv = parent.find('.resCarousel-inner'),
-        //divValueq = +parent.attr('data-value'),
         itemSpeed = +parent.attr("data-speed"),
         itemLoad = +parent.attr("data-load"),
-        //animi = parent.attr("data-animator"),
+
         translateXval = '',
         currentSlide = "",
         itemLenght = itemsDiv.find(".item").length,
@@ -257,10 +246,7 @@ function ResCarousel(Btn) {
         dataItm = +Math.round(itemsDiv.outerWidth() / itemWidth),
         cond = $(Btn).hasClass("leftLst"),
         divValue = Math.round(itemsDiv.scrollLeft() / itemWidth);
-    //console.log(dataItm + "," + Math.abs(dataItmq));
-    //console.log(divValue + "," + divValueq);
-    //console.log(cond);
-    //console.log(typeof + parent.attr("data-slide"))
+
     itemSpeed = !isNaN(itemSpeed) ? itemSpeed : 400;
     slide = slide < dataItm ? slide : dataItm;
 
@@ -268,13 +254,13 @@ function ResCarousel(Btn) {
         currentSlide = divValue - slide;
         translateXval = currentSlide * itemWidth;
         var MoveSlide = currentSlide + slide;
-        //console.log(itemloop);
+
         if (divValue == 0) {
             currentSlide = itemLenght - slide;
             translateXval = currentSlide * itemWidth;
             currentSlide = itemLenght - dataItm;
             itemSpeed = 400;
-            //console.log(currentSlide + "," + translateXval);
+
         } else if (slide >= MoveSlide) {
             currentSlide = translateXval = 0;
         }
@@ -283,9 +269,6 @@ function ResCarousel(Btn) {
         translateXval = currentSlide * itemWidth;
         var MoveSlide = currentSlide + slide;
 
-        //console.log(itemLenght + "," + (MoveSlide + "," + slide + "," + dataItm));
-        //console.log(itemLenght + "," + (MoveSlide - slide + dataItm));
-        //console.log((divValue + dataItm) + "," + itemLenght);
         if (divValue + dataItm == itemLenght) {
             currentSlide = translateXval = 0;
             itemSpeed = 400;
@@ -294,33 +277,25 @@ function ResCarousel(Btn) {
             translateXval = currentSlide * itemWidth;
             currentSlide = itemLenght - dataItm;
         }
-        // resCarouselAnimator(itemsDiv, currentSlide + 1, currentSlide + slide);
     }
-    //console.log(slide + "," + itemWidth);
+
     parent.attr("data-animator") == "lazy" && resCarouselAnimator(itemsDiv, cond ? 0 : 1, currentSlide + 1, currentSlide + dataItm, itemSpeed, (slide * itemWidth));
-    //console.log(itemsDiv.scrollLeft() + "," + translateXval)
-    //console.log(itemSpeed);
+
     if (!isNaN(itemLoad)) {
         itemLoad = itemLoad >= slide ? itemLoad : slide;
-        //console.log((itemLenght - itemLoad) <= currentSlide + dataItm);
-        //console.log((itemLenght - itemLoad) + " ," + (currentSlide + dataItm) + " ," + (itemLenght - dataItm));
         (itemLenght - itemLoad) <= (currentSlide + dataItm) && ResCarouselLoad1(itemsDiv);
     }
-    itemsDiv.animate({ scrollLeft: translateXval }, itemSpeed);
+    itemsDiv.animate({
+        scrollLeft: translateXval
+    }, itemSpeed);
     parent.attr("data-value", currentSlide);
-
-    //var t1 = performance.now();
-    //console.log('Took', (t1 - t0).toFixed(4), 'milliseconds to generate');
 }
 
-
 function ResCarouselLoad1(e) {
-    //console.log(e.attr("id"));
     $("#" + e.attr("id")).trigger("ResCarouselLoad");
 }
 
 function resCarouselAnimator(parent, direction, start, end, speed, length) {
-    //console.log(parent + "," + start + "," + end);
     var val = 5;
     if (direction == 0) {
         for (var i = start - 1; i < end + 1; i++) {
@@ -328,23 +303,35 @@ function resCarouselAnimator(parent, direction, start, end, speed, length) {
         }
         val = -val;
     }
-    //console.log(length);
-    //if (direction == 1) {
-    //    for (var i = start - 1; i < end + 1; i++) {
-    //        length = length / 2
-    //        console.log(length);
-    //    }
-    //    //val = val;
-    //}
-    //val = direction == 1 ? length : -length;
 
     for (var i = start - 1; i < end; i++) {
         val = direction == 0 ? val / 2 : val * 2;
-        //console.log(val);
-        //console.log(parent.find(".item").eq(i).find("h1").text());
         parent.find(".item").eq(i).css("transform", "translateX(" + val + "px)");
     }
-    setTimeout(function() {
+    setTimeout(function () {
         parent.find(".item").attr("style", "");
     }, speed - 70);
 }
+
+// ---------- search filter -------------
+
+function filterNames() {
+
+    $('#search-magnifier').on('click', function (e) {
+        e.preventDefault();
+       var word = $('#search_param').find('input').val();
+       console.log(word)
+       fiterByName(word);
+
+    });
+}
+
+var fiterByName = function () {
+    $.ajax({
+        type: "GET",
+        url: "/api/extensions/filter",
+        success: function (data) {
+            fillMainPageList($('#all-container'), data)
+        }
+    });
+};
