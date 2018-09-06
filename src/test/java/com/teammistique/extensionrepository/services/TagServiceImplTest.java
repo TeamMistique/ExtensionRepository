fb578e77e900cf7c238eb7ac31d9d5a98b0f2b22
biaedwards <bibby.georgieva@gmail.com>
2018-09-06 11:16:50 +0300

    Written all tests for user service

--- src/test/java/com/teammistique/extensionrepository/services/TagServiceImplTest.java
+++ src/test/java/com/teammistique/extensionrepository/services/TagServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.teammistique.extensionrepository.services;
 
-import com.teammistique.extensionrepository.data.TagSqlRepository;
 import com.teammistique.extensionrepository.data.base.TagRepository;
 import com.teammistique.extensionrepository.models.Tag;
 import org.junit.Assert;
@@ -24,17 +23,16 @@ public class TagServiceImplTest {
     public void createTag_shouldReturnTag() {
         Tag tag = new Tag();
         when(mockTagRepository.saveOrUpdate(tag)).thenReturn(tag);
-
         Tag result = tagService.createTag(tag);
-
         Assert.assertSame(tag, result);
     }
 
     @Test
-    public void getByID_shouldCallRepoMethod() {
+    public void getByID_shouldReturnResultFromRepoMethod() {
         String name = "name";
-        when(mockTagRepository.findByName(name)).thenReturn(new Tag(name));
+        Tag toReturn = new Tag(name);
+        when(mockTagRepository.findByName(name)).thenReturn(toReturn);
         Tag tag = tagService.getByName(name);
-        verify(mockTagRepository).findByName(name);
+        Assert.assertSame(toReturn, tag);
     }
 }
--- src/test/java/com/teammistique/extensionrepository/services/UserServiceImplTest.java
+++ src/test/java/com/teammistique/extensionrepository/services/UserServiceImplTest.java
@@ -2,9 +2,15 @@ package com.teammistique.extensionrepository.services;
 
 import com.teammistique.extensionrepository.data.base.UserRepository;
 import com.teammistique.extensionrepository.models.User;
+import com.teammistique.extensionrepository.models.security.Role;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -20,30 +26,88 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void findOne_shouldCallRepoMethod() {
+    public void findOne_shouldReturnRepoMethodResult() {
         String username = "username";
-        when(mockUserRepository.getUserByUsername(username)).thenReturn(new User());
+        User expected = new User();
+        when(mockUserRepository.getUserByUsername(username)).thenReturn(expected);
         User user = userService.findOne(username);
-        verify(mockUserRepository).getUserByUsername(username);
+        assertSame(expected, user);
     }
 
     @Test
-    public void findAll() {
+    public void findAll_shouldReturnRepoMethodResult() {
+        List<User> expected = Arrays.asList(
+                new User(),
+                new User(),
+                new User()
+        );
+        when(mockUserRepository.listAll()).thenReturn(expected);
+        List<User> users = userService.findAll();
+        assertSame(expected, users);
     }
 
     @Test
-    public void findById() {
+    public void findById_shouldReturnRepoMethodResult() {
+        Integer id = 12;
+        User expected = new User();
+        when(mockUserRepository.findById(id)).thenReturn(expected);
+        User user = userService.findById(id);
+        assertSame(expected, user);
     }
 
     @Test
-    public void save() {
+    public void save_shouldReturnANewUserWithBCryptedPassword() {
+        String rawPassword = "password";
+        User userToCreate = new User("username", rawPassword);
+        when(mockUserRepository.create(userToCreate)).thenReturn(userToCreate);
+
+        User toReturn = userService.save(userToCreate, new ArrayList<>());
+
+        assertTrue(bCryptPasswordEncoder.matches(rawPassword, toReturn.getPassword()));
     }
 
     @Test
-    public void changeEnabled() {
+    public void save_shouldReturnAnEnabledUser() {
+        User toCreate = new User("username", "password");
+        when(mockUserRepository.create(toCreate)).thenReturn(toCreate);
+        User toReturn = userService.save(toCreate, new ArrayList<>());
+        assertTrue(toReturn.isEnabled());
     }
 
     @Test
-    public void loadUserByUsername() {
+    public void save_shouldReturnAUserWithAListOfRoles() {
+        List<Role> roles = Arrays.asList(
+                new Role(),
+                new Role()
+        );
+        User toCreate = new User("username", "password");
+        when(mockUserRepository.create(toCreate)).thenReturn(toCreate);
+        User toReturn = userService.save(toCreate, roles);
+        assertSame(roles, toReturn.getAuthorities());
+    }
+
+    @Test
+    public void changeEnabled_shouldEnableUser_ifItWasDisabled() {
+        User toEnable = new User();
+        toEnable.setEnabled(0);
+        when(mockUserRepository.update(toEnable)).thenReturn(toEnable);
+        User returned = userService.changeEnabled(toEnable);
+        assertTrue(returned.isEnabled());
+    }
+
+    @Test
+    public void changeEnabled_shouldDisableUser_ifItWasEnabled() {
+        User toDisable = new User();
+        toDisable.setEnabled(1);
+        when(mockUserRepository.update(toDisable)).thenReturn(toDisable);
+        User returned = userService.changeEnabled(toDisable);
+        assertFalse(returned.isEnabled());
+    }
+
+    @Test
+    public void loadUserByUsername_shouldCallFindOne() {
+        String toFind = "toFind";
+        UserDetails user = userService.findOne(toFind);
+        verify(mockUserRepository).getUserByUsername(toFind);
     }
 }
